9f9b1ec2f17236baebf2a52b3c64a89e14e48cc2
Ataxexe <ataxexe@devnull.tools>
2017-02-21 20:54:14 -0300

    layout improvements

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
     });
+    setTimeout(function() {
+        $("#alert").css('visibility','visible').hide().fadeIn()
+    }, 1000)
 });
