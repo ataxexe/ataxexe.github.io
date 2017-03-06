faef5c242a607073306184f1fb368a2165710de3
Ataxexe <ataxexe@devnull.tools>
2017-03-05 23:26:29 -0300

    Randomize error messages

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,8 +1,19 @@
-$(document).ready(function() {
-    $('pre code').each(function(i, block) {
+phrases = [
+    "Error occurred successfully",
+    "Null is null or is not an object",
+    "An internal error occurred while showing an internal error",
+    "An error occurred while displaying the previous error",
+    "No error occurred",
+    "Keyboard error, press F1 to continue",
+    "Catastrophic failure"
+]
+
+$(document).ready(function () {
+    $('pre code').each(function (i, block) {
         hljs.highlightBlock(block);
     });
-    setTimeout(function() {
-        $("#alert").css('visibility','visible').hide().fadeIn()
+    setTimeout(function () {
+        $("#alert-description").text(phrases[Math.floor((Math.random() * phrases.length))])
+        $("#alert").css('visibility', 'visible').hide().fadeIn()
     }, 1000)
 });
