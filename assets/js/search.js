91f11450cde01aefea2217ca029cd4ca91ba0297
Ataxexe <ataxexe@devnull.tools>
2017-08-16 19:22:50 -0300

    remove tag search

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,14 +1,9 @@
 full_engine = lunr(function () {
-  this.field('title', { boost: 10 })
-  this.field('tags', { boost: 8 })
-  this.field('excerpt', { boost: 5 })
+  this.field('title', { boost: 4 })
+  this.field('tags', { boost: 2 })
   this.field('category')
   this.field('date')
-  this.ref('url')
-});
-
-tag_engine = lunr(function () {
-  this.field('tags')
+  this.field('language')
   this.ref('url')
 });
 
@@ -25,7 +20,6 @@ load_index = function (callback) {
       search_entries = data
       $.each(search_entries, function () {
         full_engine.add(this)
-        tag_engine.add(this)
       })
       index_loaded = true
       input.attr("placeholder", "Search...")
@@ -45,11 +39,8 @@ search = function (term, engine, callback) {
     })
   }
 }
-full_search = function (term, callback) {
+search_posts = function (term, callback) {
   search(term, full_engine, callback)
-}
-tag_search = function (tag, callback) {
-  search(tag, tag_engine, callback)
 }
 
 finish = function (result, callback) {
@@ -63,11 +54,12 @@ finish = function (result, callback) {
     index = this['ref']
     entry = search_entries[index]
     title = $("<h3 class='list-group-item-heading result-title'>" + entry['title'] + "</h3>")
-    info = $("<ul class='post-info'><li><span><i class='fa fa-calendar'></i> " + entry['date'] + " </span></li>" +
-      "<li><span><i class='fa fa-caret-right'></i> " + entry['category'] + "</span></li></ul>")
-    p = $("<p class='list-group-item-text result-excerpt'>" + entry['excerpt'] + "</p>")
+    info = $("<ul class='post-info'><li><span><i class='fa fa-calendar'></i> " + entry['date'] +
+      " </span></li>" + "<li><span><i class='fa fa-caret-right'></i> " + entry['category'] +
+      "</span></li>" + "<li><img src=\"/assets/images/lang/" + entry['language'] + ".svg\"></li>" +
+      "</ul>")
     $("<a href='" + entry['url'] + "' class='list-group-item'>")
-        .append(title, info, p)
+        .append(title, info)
         .appendTo("#search-result")
   })
   $("#search-result").show("slow")
@@ -90,16 +82,12 @@ $("#btn-search").attr("disabled", "disabled")
 $("#btn-clear-search").click(cancel_search)
 $("#btn-search").click(function () {
   term = $("#search-input").val()
-  full_search(term)
+  search_posts(term)
 })
 $("#search-input").keydown(function (event) {
   var ENTER = 13
   if (event.keyCode == ENTER) {
     term = $(this).val()
-    full_search(term)
+    search_posts(term)
   }
 }).focus(load_index)
-
-$(".post-tag").click(function () {
-  tag_search($(this).text(), scroll_up)
-})
